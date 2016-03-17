af5ac418e3f35a00204580dd0f500b8ca9c519b8
James Singleton <jsing25@users.noreply.github.com>
2016-03-16 20:13:05 -0500

    Create mergesort.h

---
+++ mergesort/mergesort.h
@@ -0,0 +1,10 @@
+#ifndef MERGESORT_H
+#define MERGESORT_H
+
+class MergeSort {
+public:
+	static void merge(int A[], int left[], int right[], int size);
+	static void mergeSort(int A[], int size);
+};
+
+#endif
